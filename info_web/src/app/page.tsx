709a446360af81943ef8444ce42e3a02fc0f73ec
mcgarretronald <mcgarretronald@gmail.com>
2024-09-11 17:03:14 +0300

    join feature

--- info_web/src/app/page.tsx
+++ info_web/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import LandingPage from "./Components/LandingPage";
 import Values from "./Components/Values";
+import Join from "./Components/Join";
 export default function Home  (){
 return (
   <div>
     <LandingPage/>
     <Values/>
+    <Join/>
   </div>
 
 )
