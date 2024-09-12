3c4ad522463acf95d25d220cbfe7609878345d77
mcgarretronald <mcgarretronald@gmail.com>
2024-09-12 14:06:13 +0300

    fixed font sizes

--- info_web/src/app/Components/Join/index.tsx
+++ info_web/src/app/Components/Join/index.tsx
@@ -12,8 +12,8 @@ export default function Join() {
                 ></Image>
             </div>
             <div className="mt-[10%] lg:mr-[20%] text-center">
-                <h1 className="text-[30px] font-bold  text-artisticblue">Join the Sustainable Fashion Movement</h1>
-                <p className="text-[20px] mt-6">Ready to make a difference? Join us in revolutionizing the fashion industry by choosing eco-friendly options and supporting a greener future. Sign up to stay updated on our latest collections, events, and more.</p>
+                <h1 className="text-[32npm px] font-bold  text-artisticblue">Join the Sustainable Fashion Movement</h1>
+                <p className="text-[16px] mt-6">Ready to make a difference? Join us in revolutionizing the fashion industry by choosing eco-friendly options and supporting a greener future. Sign up to stay updated on our latest collections, events, and more.</p>
                 <button className="bg-forestgreen text-[20px] cursor-pointer text-white w-[150px] h-[50px] rounded-[30px] mt-9">Join</button>
             </div>
         </div>
--- info_web/src/app/page.tsx
+++ info_web/src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import Join from "./Components/Join";
+import LandingPage from "./Components/LandingPage";
+import Values from "./Components/Values";
 export default function Home  (){
 return (
   <div>
+    <LandingPage/>
+    <Values/>
     <Join/>
   </div>
 
